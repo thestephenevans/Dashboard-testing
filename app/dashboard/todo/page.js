26fde64c6cd1eebebb474c6858a3c881e191cf03
Stephen-Evans-Sokada <stephenevans2706@gmail.com>
2024-03-11 16:59:57 +0000

    Update page.js

--- app/dashboard/todo/page.js
+++ app/dashboard/todo/page.js
@@ -1,13 +1,33 @@
-"use client"
-
-import React, { useState, useEffect } from "react";
+import { sql } from "@vercel/postgres";
+import { NextResponse } from 'next/server';
+//import React, { useState, useEffect } from "react";
 import Card from "@/app/ui/dashboard/card";
 
-export default function Page(){
-    const [list, setList] = useState([]);
-    const [input, setInput] = useState('');
+const handleSubmit = async (event) => {
+    'use server'
+    event.preventDefault();
+    
+    const inputValue = event.target.querySelector('input').value;
 
-    useEffect(() => {
+    if(inputValue.trim() !== ''){
+        await sql`INSERT INTO LIST (id, value, completed) VALUES (${crypto.randomUUID()}, ${inputValue}, ${false})`;
+        event.target.querySelector('input').value = '';
+    }
+};
+
+
+export default async function Page(){
+    //const [list, setList] = useState([]);
+    //const [input, setInput] = useState('');
+
+    try {
+        const { rows } = await sql`SELECT * from LIST`;
+        console.log(rows);
+    } catch (error) {
+        console.error('Error selecting rows from LIST table:', error.message);
+    }
+
+    /*useEffect(() => {
         if (typeof window !== 'undefined') {
             const storedList = JSON.parse(localStorage.getItem('todo-list'));
             if (storedList) {
@@ -15,21 +35,14 @@ export default function Page(){
                 console.log(list);
             }
         }
-    }, []);
+    }, []); */
 
-    useEffect(() => {
+    /*useEffect(() => {
         if (typeof window !== 'undefined') {
             localStorage.setItem('todo-list', JSON.stringify(list));
             console.log(list);
         }
-    }, [list]);
-
-    function handleSubmit(e){
-        e.preventDefault();
-        const newItem = { id: crypto.randomUUID(), value: input, completed: false };
-        setList([...list, newItem]);
-        setInput('');
-    }
+    }, [list]);*/
 
     return(
         <div>
@@ -41,42 +54,40 @@ export default function Page(){
                             <form onSubmit={handleSubmit}>
                                 <label htmlFor="to-do" className="text-white">Add Todo</label>
                                 <input 
-                                    value={input} 
+                                    //value={input} 
                                     type="text" 
                                     name="to-do" 
                                     className="block w-full px-4 py-2 border" 
-                                    onChange={(e) => setInput(e.target.value)} 
                                 />
-                                <button type="submit" className="block w-full px-4 py-2 mt-2 text-white bg-blue-500 hover:bg-blue-600">Add</button>
+                                <button 
+                                    type="submit" 
+                                    className="block w-full px-4 py-2 mt-2 text-white bg-blue-500 hover:bg-blue-600"
+                                >
+                                    Add
+                                </button>
                             </form>
                         </Card>
                     </div>
                     <div className="w-6/12 rounded bg-white p-5">
-                        {list.length > 0 ? 
+                        {/*list.length > 0 ? 
                             <h2 className="text-2xl font-bold mt-5">To Do:</h2> 
                         : 
-                            <span>No Items Listed</span>
+                            <span>No Items Listed</span> */
                         }
 
                         <ul className="list-none pl-0 flex flex-col gap-3">
-                            {list && list.map((item, index) => (
-                            <li key={index} className="flex flex-row gap-2 align-center">
-                                <input 
-                                    type="checkbox" 
-                                    checked={item.completed} 
-                                    onChange={(e) => {setList(list.map((i) => (i.id === item.id? {...i, completed: e.target.checked} : i)))}} 
-                                />
-                                {item.completed === true ? 
-                                    <span className="text-green-600 font-bold">{item.value}</span> 
-                                : 
-                                    <span className="text-black font-bold">{item.value}</span>
-                                }
-                                <button 
-                                    onClick={() => {setList(list.filter((i) => (i.id !== item.id)))}}
-                                >
-                                    Delete
-                                </button>
-                            </li>   
+                            {rows && rows.map(row => (
+                                <li key={row.id}>
+                                    <Card color={row.completed? 'bg-blue-400' : 'bg-gray-200'}>
+                                        <div className="flex flex-row justify-between">
+                                            <div className="w-5/12">
+                                                <Card color='bg-blue-400'>
+                                                    {row.value}
+                                                </Card>
+                                            </div>
+                                        </div>
+                                    </Card>
+                                </li>
                             ))}
                         </ul>
                     </div>
